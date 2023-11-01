063a7dc2431a6198b4f9be7708b1e1082414cd6b
Ilya Lomatsky <ilomatsky@gmail.com>
2023-11-01 19:33:40 +0600

    Creating interfaces, connecting burger styles, creating ingredient components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
 import {useState} from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import './components/Burger/Burger.css';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+import meat from './assets/meat.png';
+import cheese from './assets/cheese.png';
+import salad from './assets/salad.png';
+import bacon from './assets/bacon.png';
+
+interface IngredientItem {
+  name: string;
+  price: number;
+  image: string;
+}
+
+interface IngredientCount {
+  name: string;
+  count: number;
+}
+
+const ingredients: IngredientItem[] = [
+  {name: 'Meat', price: 80, image: meat},
+  {name: 'Cheese', price: 50, image: cheese},
+  {name: 'Salad', price: 10, image: salad},
+  {name: 'Bacon', price: 60, image: bacon},
+];
+
+const App = () => {
+  const [ingredients, setIngredients] = useState<IngredientItem[]>([
+    {name: 'Meat', count: 0},
+    {name: 'Cheese', count: 0},
+    {name: 'Salad', count: 0},
+    {name: 'Bacon', count: 0},
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo"/>
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo"/>
-        </a>
+    <div className="App">
+      <div className="Ingredients">
+        {ingredients.map((ingredient, index) => (
+          <div key={index}><img src={ingredient[index].image} alt={ingredient.name}/>{ingredient.name}{ingredient.count}</div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="Burger">
+        <div className="BreadTop">
+          <div className="Seeds1"></div>
+          <div className="Seeds2"></div>
+        </div>
+        <div className="Salad"></div>
+        <div className="Cheese"></div>
+        <div className="Meat"></div>
+        <div className="BreadBottom"></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
