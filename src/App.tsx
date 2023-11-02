4f4eb7cf96a66455b76b880edc4785a7ae595cb2
Ilya Lomatsky <ilomatsky@gmail.com>
2023-11-02 16:50:57 +0600

    Added a change in the status of ingredient quantities

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import {useState} from 'react';
+import React, { useState } from 'react';
 import './components/Burger/Burger.css';
 import './App.css';
-
 import meat from './assets/meat.png';
 import cheese from './assets/cheese.png';
 import salad from './assets/salad.png';
@@ -18,26 +17,47 @@ interface IngredientCount {
   count: number;
 }
 
+const initialIngredientCounts: IngredientCount[] = [
+  { name: 'Meat', count: 0 },
+  { name: 'Cheese', count: 0 },
+  { name: 'Salad', count: 0 },
+  { name: 'Bacon', count: 0 },
+];
+
 const ingredients: IngredientItem[] = [
-  {name: 'Meat', price: 80, image: meat},
-  {name: 'Cheese', price: 50, image: cheese},
-  {name: 'Salad', price: 10, image: salad},
-  {name: 'Bacon', price: 60, image: bacon},
+  { name: 'Meat', price: 80, image: meat },
+  { name: 'Cheese', price: 50, image: cheese },
+  { name: 'Salad', price: 10, image: salad },
+  { name: 'Bacon', price: 60, image: bacon },
 ];
 
 const App = () => {
-  const [ingredients, setIngredients] = useState<IngredientItem[]>([
-    {name: 'Meat', count: 0},
-    {name: 'Cheese', count: 0},
-    {name: 'Salad', count: 0},
-    {name: 'Bacon', count: 0},
-  ]);
+  const [ingredientCounts, setIngredientCounts] = useState<IngredientCount[]>(initialIngredientCounts);
+
+  const handleIncrement = (index: number) => {
+    const updatedCounts = [...ingredientCounts];
+    updatedCounts[index].count++;
+    setIngredientCounts(updatedCounts);
+  };
+
+  const handleDecrement = (index: number) => {
+    const updatedCounts = [...ingredientCounts];
+    if (updatedCounts[index].count > 0) {
+      updatedCounts[index].count--;
+      setIngredientCounts(updatedCounts);
+    }
+  };
 
   return (
     <div className="App">
       <div className="Ingredients">
-        {ingredients.map((ingredient, index) => (
-          <div key={index}><img src={ingredient[index].image} alt={ingredient.name}/>{ingredient.name}{ingredient.count}</div>
+        {ingredientCounts.map((ingredient, index) => (
+          <div key={index}>
+            <img src={ingredients[index].image} alt={ingredient.name} />
+            {ingredient.name} - {ingredient.count}
+            <button onClick={() => handleIncrement(index)}>Добавить</button>
+            <button onClick={() => handleDecrement(index)}>Убрать</button>
+          </div>
         ))}
       </div>
 
@@ -46,6 +66,7 @@ const App = () => {
           <div className="Seeds1"></div>
           <div className="Seeds2"></div>
         </div>
+
         <div className="Salad"></div>
         <div className="Cheese"></div>
         <div className="Meat"></div>
