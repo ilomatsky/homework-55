68345f96ccfa1d5604fe4b43e5d97ec6171cb824
Ilya Lomatsky <ilomatsky@gmail.com>
2023-11-02 20:42:51 +0600

    Brought the burger total into the layout, designed the burger styles

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const App = () => {
   const handleDecrement = (index: number) => {
     const updatedCounts = [...ingredientCounts];
     if (updatedCounts[index].count > 0) {
-      updatedCounts[index].count--;
+      updatedCounts[index].count = 0;
       setIngredientCounts(updatedCounts);
 
       const ingredientName = updatedCounts[index].name;
@@ -55,27 +55,49 @@ const App = () => {
     }
   };
 
+  const baseBurgerPrice = 30;
+
+  const calculateTotalPrice = () => {
+    const ingredientPriceSum = burgerIngredients.reduce(
+      (sum, ingredient) => {
+        const selectedIngredient = ingredients.find(item => item.name === ingredient);
+        return sum + (selectedIngredient?.price || 0);
+      },
+      0
+    );
+    return baseBurgerPrice + ingredientPriceSum;
+  };
+
+
   return (
     <div className="App">
-      <div className="Ingredients">
-        {ingredientCounts.map((ingredient, index) => (
-          <div key={index}>
-            <img onClick={() => handleIncrement(index)} src={ingredients[index].image} alt={ingredient.name}/>
-            {ingredient.name} - x{ingredient.count}
-            <button onClick={() => handleDecrement(index)}>Remove</button>
+      <div className='BurgerConstructor'>
+        <div className="Ingredients">
+          {ingredientCounts.map((ingredient, index) => (
+            <div className='ingredient' key={index}>
+              <img onClick={() => handleIncrement(index)} src={ingredients[index].image} alt={ingredient.name}/>
+              <div>
+                <strong className='ingredientName'>{ingredient.name}</strong><strong>x{ingredient.count}</strong>
+              </div>
+              <button onClick={() => handleDecrement(index)}>Clear</button>
+            </div>
+          ))}
+        </div>
+
+        <div className="Burger">
+          <div className="BreadTop">
+            <div className="Seeds1"></div>
+            <div className="Seeds2"></div>
           </div>
-        ))}
+          {burgerIngredients.map((ingredient, index) => (
+            <div key={index} className={ingredient}></div>
+          ))}
+          <div className="BreadBottom"></div>
+        </div>
       </div>
 
-      <div className="Burger">
-        <div className="BreadTop">
-          <div className="Seeds1"></div>
-          <div className="Seeds2"></div>
-        </div>
-        {burgerIngredients.map((ingredient, index) => (
-          <div key={index} className={ingredient}></div>
-        ))}
-        <div className="BreadBottom"></div>
+      <div className="TotalPrice">
+        <strong>Price: {calculateTotalPrice()}$</strong>
       </div>
     </div>
   );
